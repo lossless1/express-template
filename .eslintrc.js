c60499f697e9ab9e287962b131fe0ba42e2d6db6
Volodymyr Saakian <vsaakian@lohika.com>
2020-04-01 17:23:19 +0300

    First release

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'arrow-parens': [0],
+    'implicit-arrow-linebreak': [0],
+    'function-paren-newline': [0],
+    'import/no-dynamic-require': [0],
+    'global-require': [0],
+    'operator-linebreak': [0],
+    'object-curly-newline': [0],
+    'no-plusplus': [0],
+    'prefer-arrow-callback': [0],
+  },
+};
---
+++ README.md
@@ -0,0 +1,21 @@
+express-template
+
+# Installation
+
+`$ npm i`
+
+# Before running
+
+`$ npm run db:migrate`
+
+# Running dev
+
+`$ npm run start:dev`
+
+# Running prod
+
+`$ npm run start`
+
+## Example usage
+
+`user.http` is example http file to send and receive data from server in a moment. Its working like swagger but in vscode env.
